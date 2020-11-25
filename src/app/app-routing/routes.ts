5868c61f81649f829be9af39846cea9433dc6d54
verlopezsa <verlopezsa@gmail.com>
2020-11-25 17:29:20 +0100

    ASP Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { MenuComponent } from '../menu/menu.component';
-import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -9,6 +9,7 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'dishdetails/:id',   component: DishdetailsComponent },
   { path: 'contactus',     component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
